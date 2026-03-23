f30415ce8619bf9ad4673843a9982d724d13c591
Naveen chauhan <143591216+nav380@users.noreply.github.com>
2026-03-23 14:32:58 +0530

    commit

--- client/src/components/ContactSection.tsx
+++ client/src/components/ContactSection.tsx
@@ -49,17 +49,17 @@ export default function ContactSection() {
 
   return (
     <section id="contact" className="py-20 bg-gray-50">
-      
-      <div className="grid lg:grid-cols-3 gap-12 max-w-7xl mx-auto px-6">
+
+      <div className="grid lg:grid-cols-3 gap-12 md:max-w-7xl mx-auto px-6 max-w-[100vw]">
 
         {/* ================= LEFT — CONTACT FORM ================= */}
-        <div className="lg:col-span-2">
-          <div className="bg-red-800 p-8 rounded-2xl border border-gray-200 shadow-xl">
-            <h3 className="text-2xl text-white mb-8 font-bold font-['Archivo']">
+        <div className="lg:col-span-2 ">
+          <div className="bg-red-800 p-8 rounded-2xl border border-gray-200 shadow-xl " >
+            <h3 className="text-2xl text-white mb-8 font-bold font-['Archivo'] ">
               Send Us a Message
             </h3>
 
-            <form onSubmit={handleSubmit} className="space-y-6">
+            <form onSubmit={handleSubmit} className="space-y-6  ">
 
               {/* Name & Email */}
               <div className="grid md:grid-cols-2 gap-6">
@@ -220,13 +220,16 @@ export default function ContactSection() {
 
           {/* Google Map */}
           <div className="rounded-2xl overflow-hidden border border-gray-800 shadow-xl">
-            
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.0591621872823!2d77.17961087601311!3d28.507867689729995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1ef231229f05%3A0x920c211472263e46!2sMike%20Hospitality%20LLP!5e0!3m2!1sen!2sin!4v1771398197429!5m2!1sen!2sin" width="600" height="450"   loading="lazy" ></iframe>
+            <iframe
+              className="w-full h-[350px]"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.0591621872823!2d77.17961087601311!3d28.507867689729995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1ef231229f05%3A0x920c211472263e46!2sMike%20Hospitality%20LLP!5e0!3m2!1sen!2sin!4v1771398197429!5m2!1sen!2sin"
+              loading="lazy"
+            ></iframe>
           </div>
 
         </div>
       </div>
-    
+
     </section>
   );
 }
